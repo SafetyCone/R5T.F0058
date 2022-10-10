c9930a79e8a94c6d730c523f06e89a96653356ad
davidcoats <david@getsafetycone.com>
2022-10-09 22:45:32 -0700

    Initial commit.

---
+++ source/R5T.F0058/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0058
+{
+	/// <summary>
+	/// Repository operations (resultified and with intra-action observability) functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
